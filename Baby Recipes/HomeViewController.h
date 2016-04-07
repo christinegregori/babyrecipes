3e4c765326357a0163495e499c03048a385deedf
Christine Gregori <christinegregori@christines-macbook.local>
2016-04-06 19:44:59 -0700

    made home view controller

--- Baby Recipes/HomeViewController.h
+++ Baby Recipes/HomeViewController.h
@@ -1,5 +1,5 @@
 //
-//  FirstViewController.h
+//  HomeViewController.h
 //  Baby Recipes
 //
 //  Created by Christine Gregori on 4/6/16.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface FirstViewController : UIViewController
-
+@interface HomeViewController : UITableViewController
 
 @end
-
