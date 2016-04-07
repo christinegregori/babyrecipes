e5771ac95100de3e336e9b210ba9db4f8c9d6ca2
Christine Gregori <christinegregori@christines-macbook.local>
2016-04-06 19:54:00 -0700

    added recipe list vc

---
+++ Baby Recipes/RecipeListViewController.h
@@ -0,0 +1,13 @@
+//
+//  RecipeListViewController.h
+//  Baby Recipes
+//
+//  Created by Christine Gregori on 4/6/16.
+//  Copyright © 2016 Christine Gregori. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecipeListViewController : UITableViewController
+
+@end
---
+++ Baby Recipes/RecipeListViewController.m
@@ -0,0 +1,63 @@
+//
+//  RecipeListViewController.m
+//  Baby Recipes
+//
+//  Created by Christine Gregori on 4/6/16.
+//  Copyright © 2016 Christine Gregori. All rights reserved.
+//
+
+#import "RecipeListViewController.h"
+
+@interface RecipeListViewController ()
+
+@end
+
+@implementation RecipeListViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 5;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+    
+    UILabel *label = [cell viewWithTag:100];
+    
+    // Configure the cell...
+    //    label.text = @"X - X Age";
+    
+    return cell;
+}
+
+/*
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
+
+@end
