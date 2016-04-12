35fb25bca4b89c81769628028094de7e79f0d9e1
Christine Gregori <christinegregori@Christines-MacBook.local>
2016-04-11 20:52:18 -0700

    started on mac app

---
+++ Recipe Manager/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  Recipe Manager
+//
+//  Created by Christine Gregori on 4/11/16.
+//  Copyright © 2016 Christine Gregori. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Do any additional setup after loading the view.
+}
+
+- (void)setRepresentedObject:(id)representedObject {
+    [super setRepresentedObject:representedObject];
+
+    // Update the view, if already loaded.
+}
+
+@end
---
+++ Shared/Constants.h
@@ -0,0 +1,16 @@
+//
+//  Constants.h
+//  Baby Recipes
+//
+//  Created by Christine Gregori on 4/11/16.
+//  Copyright © 2016 Christine Gregori. All rights reserved.
+//
+
+#ifndef Constants_h
+#define Constants_h
+
+#define DBName @"recipes.recipes"
+#define MOMDName @"Recipes"
+
+
+#endif /* Constants_h */
